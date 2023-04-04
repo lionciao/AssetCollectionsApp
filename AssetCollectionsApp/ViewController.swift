c95d5f16d8091b85d2bd5e2e1c663ed203024571
lionciao <lionciao16@gmail.com>
2023-04-04 18:24:51 +0800

    Initial Commit

---
+++ AssetCollectionsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AssetCollectionsApp
+//
+//  Created by Ciao on 2023/4/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
