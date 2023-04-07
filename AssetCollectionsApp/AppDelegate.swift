5b21a1ebde6470aa431a036983d6eaa2af8b7fc7
lionciao <lionciao16@gmail.com>
2023-04-07 14:17:36 +0800

    [chore] remove storyboard

--- AssetCollectionsApp/AppDelegate.swift
+++ AssetCollectionsApp/AppDelegate.swift
@@ -10,10 +10,20 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        // TODO: coordinator
+        let initialViewController = AssetCollectionsViewController(
+            viewModel: AssetCollectionsViewModel(
+                interactor: AssetCollectionsInteractor(service: DefaultNetworkService())
+            )
+        )
+        window.rootViewController = initialViewController
+        window.makeKeyAndVisible()
+        self.window = window
         return true
     }
 
